57e90837e7969369560ec6fd5762d7b1c06ebf66
Yaspeace <mikle0201@mail.ru>
2022-09-09 00:54:18 +0300

    Добавил проект по ЛР2

---
+++ Lab2/APT1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class APT1
+    {
+        /// <summary>
+        /// Бета-параметр
+        /// </summary>
+        public double Beta { get; set; }
+
+        /// <summary>
+        /// Параметр внимательности
+        /// </summary>
+        public double Mindfulness { get; set; }
+
+        /// <summary>
+        /// Набор векторов-прототипов
+        /// </summary>
+        private List<IVector> _prototypes;
+
+        public APT1(double beta, double mindfulness)
+        {
+            Beta = beta;
+            Mindfulness = mindfulness;
+            _prototypes = new List<IVector>();
+        }
+
+        //TODO: реализовать метод кластеризации
+        public IEnumerable<Tuple<IVector, IEnumerable<IVector>>> Clasterisate(IEnumerable<IVector> attributes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lab2/IVector.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab2
+{
+    /// <summary>
+    /// Интерфейс вектора для алгоритма APT1
+    /// </summary>
+    public interface IVector
+    {
+        /// <summary>
+        /// Побитовое "И"
+        /// </summary>
+        /// <param name="vec">Операнд</param>
+        /// <returns>Результат логического "И" с векотором vec</returns>
+        IVector And(IVector vec);
+
+        /// <summary>
+        /// Значимость вектора, в частности - число ненулевых разрядов
+        /// </summary>
+        /// <returns>Значимость вектора</returns>
+        int Normal();
+    }
+}
